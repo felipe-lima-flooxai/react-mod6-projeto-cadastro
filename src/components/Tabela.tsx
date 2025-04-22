18e088d0e0989b72a5007361037026dc9c6f769c
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-22 17:24:57 -0300

    feat: tabela de clientes crua

---
+++ src/components/Tabela.tsx
@@ -0,0 +1,49 @@
+import Cliente from "@/core/Cliente"
+
+interface TabelaProps{
+    clientes: Cliente[]
+}
+
+
+
+export default function Tabela(props:any){
+
+    function renderizarCabecalho(){
+        return (
+            <table>
+                <tr>
+                    <th>Código</th>
+                    <th>Nome</th>
+                    <th>Idade</th>
+                    <th></th>
+                </tr>
+            </table>
+        )
+
+    }
+
+    function renderizarDados(){
+        return props.clientes?.map((cliente:any, i:number)=>{
+            return (
+                <tr key={cliente.id}>
+                    <td>{cliente.id}</td>
+                    <td>{cliente.nome}</td>
+                    <td>{cliente.idade}</td>
+                </tr>
+            )
+        })
+    }
+
+    return (
+        <table>
+            <thead>
+                {renderizarCabecalho()}
+            </thead>
+            <tbody>
+                {renderizarDados()}
+            </tbody>
+        </table>
+    )
+
+    
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,10 +15,26 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bruno", 28, "2"),
+    new Cliente("Carla", 45, "3"),
+    new Cliente("Daniel", 52, "4"),
+    new Cliente("Eduarda", 31, "5"),
+    new Cliente("Felipe", 40, "6"),
+    new Cliente("Gabriela", 23, "7"),
+    new Cliente("Henrique", 37, "8"),
+    new Cliente("Isabela", 29, "9"),
+    new Cliente("João", 50, "10"),
+  ];
+
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500
      to-purple-500 text-white">
-      <Layout titulo="Cadastro Simples"><span>Conteudo</span></Layout>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes}></Tabela>
+      </Layout>
     </div>
   );
 }
