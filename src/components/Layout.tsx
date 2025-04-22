5449fa86e246cd9707e5bd06d62fca71bd9a5048
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-22 18:31:00 -0300

    feat: aprimorando tabela

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout(props: any){
 
         <div className="flex flex-col w-2/3 bg-white text-gray-800 rounded-md">
             <Titulo>{props.titulo}</Titulo>
-            <div className="p-6 m-5">
+            <div className="p-6">
               {props.children}
             </div>
         </div>
--- src/core/Cliente.tsx
+++ src/core/Cliente.tsx
@@ -2,11 +2,11 @@
 
 
 export default class Cliente {
-     #id: string
+     #id: string | null;
      #nome: string
      #idade: number
 
-    constructor(nome: string, idade: number, id:string = null){
+    constructor(nome: string, idade: number, id: string | null = null){
         this.#nome = nome
         this.#idade = idade
         this.#id = id
